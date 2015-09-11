bf96594e7cbf2905797a94989d950ddb93682c56
robmadden <rob.madden@bloc.io>
2015-09-11 13:16:34 -0700

    API - Reading

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903150656) do
+ActiveRecord::Schema.define(version: 20150911194832) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20150903150656) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
